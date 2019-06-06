6fe148e828980fc90969948a165c1afbf6688422
chinahuang <1543166885@qq.com>
2019-06-06 16:09:48 +0800

    Update itertools_tee.py

--- python/programming/迭代器与生成器/itertools_tee.py
+++ python/programming/迭代器与生成器/itertools_tee.py
@@ -1,3 +1,11 @@
 """
 迭代器将消费其处理的序列，但他不会往回处理。tee提供了一个序列之上运行多个迭代器的模式
 """
+import itertools
+
+def with_head(iterable, headsize=1):
+    a, b = itertools.tee(iterable)
+    return list(itertools.islice(a, headsize)) , b
+
+seq = [1, 2, 3, 4]
+a, b = with_head(seq, 4)
